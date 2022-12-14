7f600dd40e73555377e391a8f3aee4559e2451ac
Aditya Kaushik Kota <kkaushik@euler-login-2.wacc.wisc.edu>
2022-12-13 19:16:01 -0600

    non working code for shared memory

--- src/nlm_cuda_sm/nlm_cuda_sm.cu
+++ src/nlm_cuda_sm/nlm_cuda_sm.cu
@@ -1,4 +1,4 @@
-#include "nlm.cuh"
+#include "nlm_sm.cuh"
 #include <stdio.h>
 #include <iostream>
 #include "utils.h"
@@ -6,6 +6,14 @@
 #include <cuda.h>
 
 using namespace std;
+
+void printer(float *arr, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        cout << arr[i] << endl;
+    }
+}
 
 void nlm_cuda(float *image, float *nlm_image, int pixels, int padding, int patch)
 {
@@ -17,9 +25,14 @@ void nlm_cuda(float *image, float *nlm_image, int pixels, int padding, int patch
     float *dev_gaussian_arr;
     cudaMallocManaged(&dev_gaussian_arr, patch * patch * sizeof(float));
 
-    cudaMemcpy(&dev_gaussian_arr, gaussian_arr, sizeof(float) * pixels * pixels, cudaMemcpyHostToDevice);
+    for (int i = 0; i < patch * patch; i++)
+    {
+        dev_gaussian_arr[i] = gaussian_arr[i];
+    }
 
-    nlm_sm<<<pixels, pixels>>>(nlm_image, image, size_with_padding, dev_gaussian_arr);
+    int shared_memory_size = patch * (pixels + 2 * padding);
+
+    nlm_sm<<<pixels, pixels, shared_memory_size>>>(nlm_image, image, size_with_padding, dev_gaussian_arr, pixels, padding, patch);
     cudaDeviceSynchronize();
 
     free(gaussian_arr);
@@ -41,13 +54,17 @@ int main(int argc, char *argv[])
 
     float *dev_image;
     cudaMallocManaged((void **)&dev_image, size_with_padding * sizeof(float));
-    cudaMemcpy(&dev_image, image, sizeof(float) * size_with_padding, cudaMemcpyHostToDevice);
+
+    for (int i = 0; i < size_with_padding; i++)
+    {
+        dev_image[i] = image[i];
+    }
 
     float *nlm_image;
     cudaMallocManaged((void **)&nlm_image, size_with_padding * sizeof(float));
     for (int i = 0; i < size_with_padding; i++)
     {
-        filtered_image[i] = (float)-1;
+        nlm_image[i] = (float)-1;
     }
 
     nlm_cuda(dev_image, nlm_image, pixels, padding, patch);
--- src/nlm_cuda_sm/nlm_cuda_sm.sh
+++ src/nlm_cuda_sm/nlm_cuda_sm.sh
@@ -2,11 +2,11 @@
 
 
 #SBATCH -p wacc
-#SBATCH -J nlm_cuda
-#SBATCH -o nlm_cuda.out -e nlm_cuda.err
+#SBATCH -J nlm_cuda_sm
+#SBATCH -o nlm_cuda_sm.out -e nlm_cuda_sm.err
 #SBATCH --gres=gpu:1
 
-
+make clean
 module load nvidia/cuda
-nvcc nlm_cuda.cu nlm.cu -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o nlm_cuda
-./nlm_cuda 64 3 noisy_image.txt
+nvcc nlm_cuda_sm.cu nlm_sm.cu utils.cpp -Xcompiler -O3 -Xcompiler -Wall -Xptxas -O3 -std=c++17 -o nlm_cuda_sm
+./nlm_cuda_sm 64 3 noisy_image.txt
